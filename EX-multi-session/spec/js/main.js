32a35ebdae9ae40954ea36092cff6cc82e2a0a4d
geraw <gera.weiss@gmail.com>
2023-12-31 19:45:35 +0200

    Fix diagonal game ending in tic-tac-toe

--- EX-multi-session/spec/js/main.js
+++ EX-multi-session/spec/js/main.js
@@ -28,29 +28,57 @@ const URL = 'https://demo.prestashop.com/#/en/login'
 
 
 // Create the event array
+
+// const players = ["X", "O"];
+
+
+// const cells = [
+//     { row: 0, col: 0 }, { row: 0, col: 1 }, { row: 0, col: 2 },
+//     { row: 1, col: 0 }, { row: 1, col: 1 }, { row: 1, col: 2 },
+//     { row: 2, col: 0 }, { row: 2, col: 1 }, { row: 2, col: 2 },
+// ]
+
+// const moves = [
+//     Event("X", { row: 0, col: 0 }), Event("X", { row: 0, col: 1 }), Event("X", { row: 0, col: 2 }),
+//     Event("X", { row: 1, col: 0 }), Event("X", { row: 1, col: 1 }), Event("X", { row: 1, col: 2 }),
+//     Event("X", { row: 2, col: 0 }), Event("X", { row: 2, col: 1 }), Event("X", { row: 2, col: 2 }),
+//     Event("O", { row: 0, col: 0 }), Event("O", { row: 0, col: 1 }), Event("O", { row: 0, col: 2 }),
+//     Event("O", { row: 1, col: 0 }), Event("O", { row: 1, col: 1 }), Event("O", { row: 1, col: 2 }),
+//     Event("O", { row: 2, col: 0 }), Event("O", { row: 2, col: 1 }), Event("O", { row: 2, col: 2 }),
+// ];
+
+
+// The size of the board
+const N = 2
+
+// An array of the two players in the game.
 const players = ["X", "O"];
 
-const cells = [
-    { row: 0, col: 0 }, { row: 0, col: 1 }, { row: 0, col: 2 },
-    { row: 1, col: 0 }, { row: 1, col: 1 }, { row: 1, col: 2 },
-    { row: 2, col: 0 }, { row: 2, col: 1 }, { row: 2, col: 2 },
-]
+// An array of all the cells in the board. Each cell is an object with a row and a column.
+const cells = [];
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+        cells.push({ row: i, col: j });
+    }
+}
 
-const moves = [
-    Event("X", { row: 0, col: 0 }), Event("X", { row: 0, col: 1 }), Event("X", { row: 0, col: 2 }),
-    Event("X", { row: 1, col: 0 }), Event("X", { row: 1, col: 1 }), Event("X", { row: 1, col: 2 }),
-    Event("X", { row: 2, col: 0 }), Event("X", { row: 2, col: 1 }), Event("X", { row: 2, col: 2 }),
-    Event("O", { row: 0, col: 0 }), Event("O", { row: 0, col: 1 }), Event("O", { row: 0, col: 2 }),
-    Event("O", { row: 1, col: 0 }), Event("O", { row: 1, col: 1 }), Event("O", { row: 1, col: 2 }),
-    Event("O", { row: 2, col: 0 }), Event("O", { row: 2, col: 1 }), Event("O", { row: 2, col: 2 }),
-];
+// An array of all the possible moves. Each move is an event with a name (X or O) and a data object with a row and a column.
+const moves = [];
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+        players.forEach(player => {
+            moves.push(Event(player, { row: i, col: j }));
+        });
+    }
+}
 
-
+// A b-thread that continously requests all the moves in the moves array.
 bthread("Continously eequest all the moves ", function () {
     while (true)
         sync({ request: moves })
 })
 
+// A b-thread that enforce the alternation between the players.
 bthread("X begins and then the players play alternatingly", function () {
     while (true) {
         sync({ waitFor: EventSet("Any X Move", e => e.name == "X"), block: EventSet("Any O Move", e => e.name == "O") })
@@ -58,6 +86,7 @@ bthread("X begins and then the players play alternatingly", function () {
     }
 })
 
+// A b-thread that enforce that a cell cannot be marked more than once.
 cells.forEach(cell => {
     bthread("A cell cannot be marked more than once", function () {
         sync({ waitFor: EventSet(`Any move in ${cell}`, e => e.data.row == cell.row && e.data.col == cell.col) })
@@ -65,49 +94,57 @@ cells.forEach(cell => {
     })
 })
 
-
+// B-threads that enforce that if a player fills a row, column or diagonal, the game ends.
 players.forEach(player => {
-    [0, 1, 2].forEach(i => {
+    [0,1].forEach(i => {
         bthread(`If a player ${player} fills row ${i}, the game ends`, function () {
-            sync({ waitFor: EventSet(`Any ${player} move in row ${i}`, e => e.name == player && e.data.row == i) })
-            sync({ waitFor: EventSet(`Any ${player} move in row ${i}`, e => e.name == player && e.data.row == i) })
-            sync({ waitFor: EventSet(`Any ${player} move in row ${i}`, e => e.name == player && e.data.row == i) })
+            for (let j = 0; j < N; j++)
+                sync({ waitFor: EventSet(`Any ${player} move in row ${i}`, e => e.name == player && e.data.row == i) })
+
             sync({ request: Event("End", { winner: player }), block: moves })
         })
 
         bthread(`If a player ${player} fills column ${i}, the game ends`, function () {
-            sync({ waitFor: EventSet(`Any ${player} move in column ${i}`, e => e.name == player && e.data.col == i) })
-            sync({ waitFor: EventSet(`Any ${player} move in column ${i}`, e => e.name == player && e.data.col == i) })
-            sync({ waitFor: EventSet(`Any ${player} move in column ${i}`, e => e.name == player && e.data.col == i) })
+            for (let j = 0; j < N; j++)
+                sync({ waitFor: EventSet(`Any ${player} move in column ${i}`, e => e.name == player && e.data.col == i) })
+
             sync({ request: Event("End", { winner: player }), block: moves })
         })
     })
 })
 
-players.forEach(player => {
-    bthread(`If a player ${player} fills the main diagonal, the game ends`, function () {
-        sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == e.data.col) })
-        sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == e.data.col) })
-        sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == e.data.col) })
-        sync({ request: Event("End", { winner: player }), block: moves })
-    })
-})
+// players.forEach(player => {
+//     bthread(`If a player ${player} fills the main diagonal, the game ends`, function () {
+//         for (let i = 0; i < N; i++)
+//             sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == e.data.col) })
+
+//         sync({ request: Event("End", { winner: player }), block: moves })
+//     })
+// })
 
 
-players.forEach(player => {
-    bthread(`If a player ${player} fills the secondary diagonal, the game ends`, function () {
-        sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == 2 - e.data.col) })
-        sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == 2 - e.data.col) })
-        sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == 2 - e.data.col) })
-        sync({ request: Event("End", { winner: player }), block: moves })
-    })
-})
+// players.forEach(player => {
+//     bthread(`If a player ${player} fills the secondary diagonal, the game ends`, function () {
+//         for (let i = 0; i < N; i++)
+//             sync({ waitFor: EventSet(`Any ${player} move on the main diagonal`, e => e.name == player && e.data.row == 2 - e.data.col) })
+
+//         sync({ request: Event("End", { winner: player }), block: moves })
+//     })
+// })
 
 bthread("No events after End", function () {
     sync({ waitFor: EventSet("End", e => e.name == "End") })
     sync({ block: moves })
 })
 
+
+
+
+// bthread("length", function () {
+//     for (let i = 0; i < 5; i++)
+//         sync({ waitFor: bp.all })
+//     sync({ block: bp.all })
+// })
 
 
 
